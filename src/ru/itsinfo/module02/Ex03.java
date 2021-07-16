ed4aa3400ad1427e7131ce760d1ba705bec5c7cc
Levgenix <mr.lobach@gmail.com>
2021-07-16 21:54:30 +0300

    Description of the task 2.3 added

--- src/ru/itsinfo/module02/Ex03.java
+++ src/ru/itsinfo/module02/Ex03.java
@@ -2,6 +2,16 @@ package ru.itsinfo.module02;
 
 import java.util.function.BinaryOperator;
 
+/**
+ * Write a lambda expression that accepts two integers arguments and returns max of them.
+ * Try not to use the Math library.
+ * Solution format. Submit your lambda expression in any valid format with ; on the end.
+ *
+ * Examples: (x, y) -> x + y; (x) -> { return x; };
+ *
+ * Sample Input: 1 4
+ * Sample Output: 4
+ */
 public class Ex03 {
     public static void main(String[] args) {
         BinaryOperator<Integer> maxFunc = (x, y) -> x > y ? x : y;
